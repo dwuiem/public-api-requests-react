96feb0fae49780961cb9a55487c8633e7bf75e1f
Pyaterikov Denis <denisladkiy@gmail.com>
2025-12-05 20:26:02 +0300

    Build docs for GitHub Pages (outDir=docs)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/public-api-requests-react/',
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  },
   server: {
     port: 3000,
     open: true
